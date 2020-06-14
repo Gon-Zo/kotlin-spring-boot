c8ee64a66523ae6075d8e290d4984e7cc0ffe1e8
gonzo <m165252@daum.net>
2020-06-14 18:53:32 +0900

    #002 DataBase Setting

---
+++ api/src/test/kotlin/com/gonzo/api/Jasypt.kt
@@ -0,0 +1,17 @@
+package com.gonzo.api
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor
+
+class Jasypt
+
+fun main(args: Array<String>) {
+    val jasypt = StandardPBEStringEncryptor()
+    jasypt.setPassword("")
+    jasypt.setAlgorithm("PBEWithMD5AndDES")
+
+    val encryptedText = jasypt.encrypt("1234")
+    val plainText = jasypt.decrypt(encryptedText)
+
+    println("encryptedText:  $encryptedText")
+    println("plainText:  $plainText")
+}
