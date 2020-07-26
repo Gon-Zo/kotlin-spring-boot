349b3b5172102080781fe67beac479292b88db8f
gonzo <m165252@daum.net>
2020-07-26 19:48:44 +0900

    Jpa 설정

---
+++ api/src/main/kotlin/com/gonzo/api/config/JpaConfiguration.kt
@@ -0,0 +1,25 @@
+package com.gonzo.api.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.support.JpaRepositoryFactory
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+
+/**
+ * Create by pnw1517@gmail.com on 2020-07-26, 일
+ * Blog : https://zzz-oficial.tistory.com
+ * Github : https://github.com/Gon-Zo
+ */
+@Configuration
+class JpaConfiguration {
+
+    @PersistenceContext
+    private lateinit var entityManager : EntityManager
+
+    @Bean
+    fun setJpaRepositoryFactory() : JpaRepositoryFactory{
+       return JpaRepositoryFactory(this.entityManager)
+    }
+
+}
