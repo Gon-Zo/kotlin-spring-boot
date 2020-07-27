c7d3236ec7d5f9f495b93ad7ed3e618ab3eeae34
gonzo <m165252@daum.net>
2020-07-27 19:51:07 +0900

    User domain crud 개발

--- api/src/main/kotlin/com/gonzo/api/web/dto/UserDto.kt
+++ api/src/main/kotlin/com/gonzo/api/web/dto/UserDto.kt
@@ -2,6 +2,11 @@ package com.gonzo.api.web.dto
 
 import com.gonzo.api.domain.user.User
 
+/**
+ * Create by pnw1517@gmail.com on 2020-07-27, 월
+ * Blog : https://zzz-oficial.tistory.com
+ * Github : https://github.com/Gon-Zo
+ */
 data class UserDto (val email : String = "" , val password: String = "") {
     fun toEntity(): User {
         return User(this.email, this.password)
